83c2eca1d6c7253cc4f83e47b22a115e04028567
Razim Rafi <8910razim@gmail.com>
2024-11-25 19:59:01 +0530

    fixed bug

--- controller/appController.js
+++ controller/appController.js
@@ -5,15 +5,5 @@ export const homePage = ( req, res) => {
 }
 
 export const registerPage = async (req, res) => {
-    try {
-        const docRef = await firestore.collection('users').add({
-             name:"razim",
-             email:"testing@gmail.com",
-            createdAt: admin.firestore.FieldValue.serverTimestamp()
-        });
-        res.status(200).json({ message: 'User added', id: docRef.id });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
     res.render('Registration.ejs')
 }
